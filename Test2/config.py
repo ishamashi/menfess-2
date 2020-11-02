b42d59e1db9bfbfe996287f92ec4626d358cd4e7
ichoprisma <icho@prisma-ads.com>
2020-11-02 16:10:52 +0700

    test2

---
+++ Test2/config.py
@@ -0,0 +1,6 @@
+consumer_key = "ddWUz5t7BuyxBy4WOiBwhvZxH"
+consumer_secret = "G4oNxUh2LxzIQNS7EUsHoZ3mNRTeCb2btdlBec8e0Ctvcc8jpe"
+access_token = "2929656734-yMMTn3gTy5ogXdJTlJFfLhvnKBCiZsW6ouC8Z48"
+access_token_secret = "Jthjxc9oAK8HJtilvfyA9rMXEUu5GdEsLBuPdncLDAy6l"
+trigger = "[cho]"
+timezone = 7 #change this into your timezone. for example if your timezone is GMT+7 you put 7
